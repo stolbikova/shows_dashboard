a39fdfefc9a7503586dc03f7fe696d8d04eb918b
Alina <alina.stolbikova@gmail.com>
2024-09-09 11:54:38 +0200

    returned http

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
-const BASE_API = "https://api.tvmaze.com/";
+const BASE_API = "http://api.tvmaze.com/";
+const BASE_API_HTTPS = "https://api.tvmaze.com/";
 
 export const GET_SHOW_DETAILS_API = (showId: string) =>
   `${BASE_API}shows/${showId}`;
@@ -6,7 +7,7 @@ export const GET_SHOW_DETAILS_API = (showId: string) =>
 export const GET_EPISODES_API = (showId: string) =>
   `${BASE_API}shows/${showId}/episodes`;
 
-export const GET_SEARCH = (q: string) => `${BASE_API}search/shows?q=${q}`;
+export const GET_SEARCH = (q: string) => `${BASE_API_HTTPS}search/shows?q=${q}`;
 
 export const GET_EPISODE_DETAILS_API = (episodeId: string) =>
   `${BASE_API}episodes/${episodeId}`;
